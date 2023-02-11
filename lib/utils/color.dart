3c34b2b0cf66c538e69b16248cf3ccc78043209f
Umang Kaklotar <umangkaklotar02@gmail.com>
2023-02-12 01:38:20 +0530

    UK || 12/02/2023 01:38 AM

--- lib/utils/color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyColors {
-  static Color bgContainer = const Color(0xff30315D);
-  static Color white = Colors.white;
-  static Color grey = Colors.grey;
-}
